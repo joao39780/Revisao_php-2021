b615d4d67b3cc6587636dca0ffe103b594b4db98
joao39780 <joao39780@uni9.edu.br>
2021-08-25 17:11:58 -0300

    Exemplo3.1.php was added

---
+++ Estrutura_condicional_Estrutura_de_repeticao/Exemplo3.1.php
@@ -0,0 +1,28 @@
+<?php 
+/*Exemplo 3.1 - Tomando uma decisão com if() */
+//a estrutura if() avalia a expressão de teste que se econtra entre parênteses, se o valor da expressão de teste for avaliado como
+//verdadeiro a instrução do bloco de código será executada
+//se a expressão não for verdadeira  o programa continuará a executas as instruções após o fechamento das chaves
+$logged_in = true;
+if($logged_in) //expressão de teste
+{
+	print "Welcome aboard trusted user"; //instrução do bloco de código
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
