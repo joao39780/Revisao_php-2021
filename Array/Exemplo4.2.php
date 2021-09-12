c099d69013d63a5842a8e9aef1d680e007c8c8fb
joao39780 <joao39780@uni9.edu.br>
2021-09-12 15:04:18 -0300

    Exemplo4.2 has been added

---
+++ Array/Exemplo4.2.php
@@ -0,0 +1,28 @@
+<?php 
+/*Exemplo4.2 - Usando sintaxe curta dos array*/
+
+//Uma abreviação que é usada para a estrutura array() é um par de colchetes (a chamada sintaxe curta dos arrays) 
+
+$vegetables = ['corn' => 'yellow', 'beet' => 'red', 'carrot' => 'orange'];
+
+
+$dinner = [0 => 'Sweet Corn and Aspargus',
+		   1 => 'Lemon Chicken',
+		   2 => 'Braised Bamboom Fungus'];
+
+
+
+$computers = ['trs-80' => 'Radio Shack', 2600 => 'Atari', 'Adam' => 'Coleco'];
+
+
+
+
+
+
+
+
+
+
+
+
+?>
