5d4eaa0221b34813b6a5ff4bcfce2ce8b9e51dd0
joao39780 <joao39780@uni9.edu.br>
2021-09-15 22:33:50 -0300

    New folder about php functions

---
+++ Functions_and_files/Exemplo5.1.php
@@ -0,0 +1,34 @@
+<?php 
+/*Exemplo5.1 - Declarando uma função*/
+
+//Para criar uma nova função, use a palavra-chave function, seguida pelo nome da função e depois, dentro de chaves, pelo corpo da função.
+
+function page_header(){
+
+print '<html><head><tittle>Welcome to my site</tittle></head>';
+
+print '<body bgcolor = "#ffffff">';
+
+}
+
+
+page_header();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
