6d3f64bea82f9f8af096c9fd99bd4051cdad01c7
joao39780 <joao39780@uni9.edu.br>
2021-09-30 22:19:29 -0300

    nw

---
+++ Orientacao_a_objetos/Exemplo6.14.php
@@ -0,0 +1,30 @@
+<?php 
+/*Exemplo6.14 - Definindo uma classe em um namespace*/
+
+namespace Tiny;
+
+class fruit{
+
+	public static function munch($bite){
+
+		print "Here is a tiny munch of $bite ";
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
