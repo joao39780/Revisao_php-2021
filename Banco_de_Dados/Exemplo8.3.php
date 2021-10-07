3fbca1274f3b31416f5b51dc0cc41d920820be82
joao39780 <joao39780@uni9.edu.br>
2021-10-07 18:04:29 -0300

    nw

---
+++ Banco_de_Dados/Exemplo8.3.php
@@ -0,0 +1,31 @@
+<?php 
+
+require_once 'Exemplo8.2.php';
+
+try{
+
+
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$q = $db->exec("CREATE TABLE dishes(
+
+					dish_id INT,
+					dish_name VARCHAR(255),
+					price DECIMAL(4,2),
+					is_spicy INT
+
+
+				   )");
+}catch(PDOException $e){
+
+	print "Could not create table: " . $e->getMessage();
+}
+
+
+
+
+
+
+
+
+
+?>
