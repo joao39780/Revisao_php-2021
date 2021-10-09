abc8883c7483f757f30de25a53ce42a8f10a2754
joao39780 <joao39780@uni9.edu.br>
2021-10-09 04:46:16 -0300

    nw

--- Banco_de_Dados/Exemplo8.3.php
+++ Banco_de_Dados/Exemplo8.3.php
@@ -8,7 +8,7 @@ try{
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$q = $db->exec("CREATE TABLE dishes(
 
-					dish_id INT,
+					dish_id INT PRIMARY KEY AUTO_INCREMENT,
 					dish_name VARCHAR(255),
 					price DECIMAL(4,2),
 					is_spicy INT
