95017f19cb7c016b43d242789ec54b0867929049
joao39780 <joao39780@uni9.edu.br>
2021-10-11 17:24:52 -0300

    nw

---
+++ Banco_de_Dados/Exemplo8.16.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once 'Exemplo8.2.php';
+
+try{
+
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//Remove pratos caros
+	$make_things_cheaper = true;
+	//Remove pratos caros
+	if($make_things_cheaper){
+		$db->exec("DELETE FROM dishes WHERE price > 19.95");
+	}else{
+		//Ou remove todos os pratos
+		$db->exec("DELETE FROM dishes");
+	}
+
+}catch(PDOException $e){
+
+	print "Couldn't delete rows: " . $e->getMessage();
+}
+
+?>
