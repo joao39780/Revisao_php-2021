cb8cc5b4a9d66db46fa61e69fa383808e123f19e
joao39780 <joao39780@uni9.edu.br>
2021-10-11 23:06:17 -0300

    nw

--- Banco_de_Dados/Exemplo8.1.php
+++ Banco_de_Dados/Exemplo8.1.php
@@ -1,6 +1,6 @@
 <?php 
 
-
+$db = new PDO('mysql:host=localhost;dbname=db_restaurant', 'admin', 'admin');
 
 
 ?>
---
+++ Banco_de_Dados/Exemplo8.2.php
@@ -0,0 +1,14 @@
+<?php 
+
+try{
+
+	$db = new PDO('mysql:host=localhost;dbname=db_restaurant', 'admin', 'admin');
+
+}catch(PDOException $e){
+
+	print "Could not connect to the database: " . $e->getMessage();
+}
+
+
+
+?>
