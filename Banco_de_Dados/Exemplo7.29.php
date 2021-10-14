53c289b20ecdcac43c1e2c463cce865c5e400a36
joao39780 <80215258+joao39780@users.noreply.github.com>
2021-10-14 19:29:42 -0300

    Update Exemplo7.29.php

--- Banco_de_Dados/Exemplo7.29.php
+++ Banco_de_Dados/Exemplo7.29.php
@@ -1,78 +1,146 @@
 <?php
-
-class FormHelper{
-
+class FormHelper
+{
 	protected $values = array();
 
-	public function __construct($values = array()){
-
-		if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	public function __construct($values = array())
+	{
+		if($_SERVER['REQUEST_METHOD'] = 'POST')
+		{
 			$this->values = $_POST;
-		}else{
+		}
+		else
+		{
 			$this->values = $values;
-		}
+		}	
+
 	}
 
-	public function input($type, $attributes = array(), $isMultiple = false){
-
+	public function input($type, $attributes = array(), $isMultiple = false)
+	{
 		$attributes['type'] = $type;
-		if(($type == 'radio') || ($type == 'checkbox')){
-			if($this->isOptionSelected($attributes['name'] ?? null, $attributes['value'] ?? null)){
+		if(($type == 'radio') || ($type == 'checkbox'))
+		{
+			if($this->isOptionSelected($attributes['name'] ?? null,
+									   $attributes['value'] ?? null)){
+
 				$attributes['checked'] = true;
-			}	
-		}
-		return $this->tag('input', $attributes, $isMultiple);
-	}
-
-
-
-	public function select($options, $attributes = array()){
-
-		$multiple = $attributes['multiple'] ?? false;
-
-		return
+			}
+				}
 		
-		$this->start('select', $attributes, $multiple) .
-		$this->options($attributes['name'] ?? null, $options).
-		$this->end('select');
+			return $this->tag('input', $attributes, $isMultiple);
+
+		}
+
+
+	public function select($options, $attributes = array())	
+	{
+		$multiple = $attributes['multiple'] ?? false;
+		return
+			$this->start('select', $attributes, $multiple) .
+			$this->options($attributes['name'] ?? null, $options) .
+			$this->end('select');
 	}
 
-
-	public function textarea($attributes = array()){
-
+	public function textarea($attributes = array())
+	{
 		$name = $attributes['name'] ?? null;
-		$value = $this->values['name'];
-		return $this->start('textarea', $attributes).
-		htmlentities($value).
-		$this->end('textarea');
-
+		$value = $this->values[$name] ?? '';
+		return $this->start('textarea', $attributes) .
+			htmlentities($value) .
+			$this->end('texarea');
 	}
 
-
-	public function tag($tag, $attributes = array(), $isMultiple = false){
-		return "<$tag {$this->attributes($attributes, $isMultiple)} />";	
+	public function tag($tag, $attributes = array(), $isMultiple = false)
+	{
+		return "<$tag {$this->attributes($attributes, $isMultiple)} />";
 	}
 
-
-	public function start($tag, $attributes = array(), $isMultiple = false){
-
-		$valueAttribute = (! (($tag == 'select') || ($tag == 'textarea')));
+	public function start($tag, $attributes = array(), $isMultiple = false)
+	{
+		//As tags <select> e <textarea> não recebem atributos de valor
+		$valueAttribute = (!(($tag == 'select') || ($tag == 'textarea')));
 		$attrs = $this->attributes($attributes, $isMultiple, $valueAttribute);
 		return "<$tag $attrs>";
 	}
-
-
-	public function end($tag){
+	
+	public function end($tag)
+	{
 		return "</$tag>";
 	}
 
+	protected function attributes($attributes, $isMultiple, $valueAttribute = true)
+	{
+		$tmp = array();
+		//Se essa tag puder incluir um atributo de valor, tiver um nome e houver uma entrada para o nome no array values, define um atributo de valor
+		if($valueAttribute && isset($attributes['name']) &&
+			array_key_exists($attributes['name'], $this->values)){
+		$attributes['value'] = $this->values[$attributes['name']];
+		}
+			
+		foreach($attributes as $k => $v)
+		{
+			//O valor booleano verdadeiro significa um atributo booleano
+			if(is_bool($v))
+			{
+				if ($v) {$tpm[] = $this->encode($k);}
+			}
+			//caso contrário, k=v
+			else
+			{
+				$value = $this->encode($v);
+				//Se esse elemento puder ter mais de um valor acrescent [] ao seu nome
 
-	protected function attributes($attributes, $isMultiple, $valueAttribute = true){
+				if($isMultiple && ($k == 'name'))
+				{
+					$value .= '[]';
+				}
+				$tmp[] = "$k=\"$value\"";
+			}
+		}	
+		return implode('', $tmp);
+	}
 
-		$
+	protected function options($name, $options)
+	{
 
+		$tmp = array();
+		foreach($options as $k => $v)
+		{
+			$s = "<option value=\"{$this->encode}\"";
+			if($this->isOptionSelected($name, $k))
+			{
+				$s .= 'selected';
+			}
+			$s .= ">{$this->encode($v)}</otpion>";
+			$tmp[] = $s;
+			}
+			return implode('', $tmp);
+		}
 
-	}	
+		protected function isOptionSelected($name, $value)
+		{
+			//Se não houver uma entrada para $name no array values, essa opção não poderá ser selecionada
+			if(! isset($this->values[$name]))
+			{
+				return false;
+			}
+		
+			else if(is_array($this->values[$name]))
+			{
+				return in_array($value, $this->values[$name]);
+			}
+			
+			else
+			{
+				return $value == $this->values[$name];
+			}	
+		}
 
+		public function encode($s)
+		{
+			return htmlentities($s);
+		}
 }
+
 ?>
