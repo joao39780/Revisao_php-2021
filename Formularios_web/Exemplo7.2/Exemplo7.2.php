4aa3909d9afcb805c054e3bcbabbf281f5b8c9b2
joao39780 <joao39780@uni9.edu.br>
2022-01-18 16:36:39 -0300

    nw

---
+++ Formularios_web/Exemplo7.2/Exemplo7.2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST" action="catalog.php">
+Product Id:
+<br>
+<input type="text" name="product_id">
+<select name="category">
+    <option value="ovenmitt">Pot Holder</option>
+    <option value="fryingpan">Frying Pan</option>
+    <option value="torch">Kitchen Torch</option>
+</select>
+<input type="submit" value="submit">
+</form>
+</body>
+</html>
