0087cb0e354e26f5b91b0098f2f43b641fd2fd0b
joao39780 <joao39780@uni9.edu.br>
2022-02-04 20:32:34 -0300

    nw

---
+++ Formularios_web/test.php
@@ -0,0 +1,9 @@
+<?php
+$array_styles = [
+    'logan paul' => ['height' => '1,85', 'weight' => '85kg'],
+    'Windows' => ['height' => '1,70', 'weight' => '78kg']
+];
+
+foreach($array_styles['logan paul'] as $features){
+    print "You have $features. <br>";
+}
