50eb9aed2add26de56bf3100c3c2d611d99e1a0c
joao39780 <joao39780@uni9.edu.br>
2022-02-04 21:11:00 -0300

    nw

---
+++ Formularios_web/Exemplo7.6.php
@@ -0,0 +1,23 @@
+<?php
+// Lógica para a execução da ação de acordo com
+// o método solicitado
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    process_form();
+}else{
+    show_form();
+}
+
+function process_form(){
+    print "Hello, ". $_POST['my_name'];
+}
+
+//Exibe o formulário
+function show_form(){
+    print<<<_HTML_
+    <form method="POST" action="$_SERVER[PHP_SELF]">
+    Your name: <input type="text" name="my_name">
+    <br>
+    <input type="submit" value="Say Hello">
+    </form>
+    _HTML_;
+}
